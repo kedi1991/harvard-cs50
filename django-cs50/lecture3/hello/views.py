ede6ba5ce0046b172fcb9010205ddc16a039fe1d
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-08-25 00:16:37 +0200

    Added an htm using render method

--- django-cs50/lecture3/hello/views.py
+++ django-cs50/lecture3/hello/views.py
@@ -6,7 +6,8 @@ def index(request):
     return HttpResponse("hello jayden")
 
 def family(request):
-    return HttpResponse("Family of 3 people")
+    #return HttpResponse("Family of 3 people")
+    return render(request, "hello/family.htm")
 
 def greeting(request, name):
     return HttpResponse(f"Hello  {name}")
