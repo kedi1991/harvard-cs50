66f56fc73a274f2e3ddf5172e9dfb6fb7575ea9b
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-08-26 02:14:05 +0200

    add list and context to the task app

--- django-cs50/lecture3/tasks/views.py
+++ django-cs50/lecture3/tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+tasks = ["update uganda code", "update zambia code", "plan new stategy for africa"]
 # Create your views here.
 def listtasks(request):
-    return render(request, "tasks/listtasks.html")
+    return render(request, "tasks/listtasks.html", {"tasks": tasks})
