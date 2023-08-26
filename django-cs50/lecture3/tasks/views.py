e9caa0b0ecf3f3897fa120348d856d4e8519fe7f
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-08-26 03:40:11 +0200

    add forms with client side validation

--- django-cs50/lecture3/tasks/views.py
+++ django-cs50/lecture3/tasks/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = ["update uganda code", "update zambia code", "plan new stategy for africa"]
+
+#my form
+class AddTaskForm(forms.Form):
+    task = forms.CharField(label="Task name: ")
+    priority = forms.IntegerField(label="Priority: ", max_value= 5)
+
 # Create your views here.
 def listtasks(request):
     return render(request, "tasks/listtasks.html", {"tasks": tasks})
 
 def addtask(request):
-    return render(request, "tasks/addtask.html")
+    return render(request, "tasks/addtask.html", {"form": AddTaskForm()})
