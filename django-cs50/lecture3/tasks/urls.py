ce5e4b23c0d93be7d8c0a9abc843826104c47651
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-08-26 03:13:03 +0200

    correct name space collusion in urls

--- django-cs50/lecture3/tasks/urls.py
+++ django-cs50/lecture3/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
     path('index', views.listtasks, name="tasks"),
     path("add", views.addtask, name="addtask"),
