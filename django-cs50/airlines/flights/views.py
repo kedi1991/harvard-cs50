63d3c8536b396427b3b642f8de918cabb1e8cec0
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-08-27 03:20:49 +0200

    Admin console configuration and additon of views to transfer data

--- django-cs50/airlines/flights/views.py
+++ django-cs50/airlines/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from  .models import Flight
+ 
 def index(request):
-    return render(request, "flights/index.html")
+    return render(request, "flights/index.html", {"flights": Flight.objects.all()})
